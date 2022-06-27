c9688ca360fa7896312fef68c47c5c37dae7da4c
Shawn <60294318+Shawn-sudo@users.noreply.github.com>
2022-06-26 20:33:13 -0400

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Atrable Web`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Atrable`,
+    siteUrl: `https://www.atrable.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
