aa8c92aa12d2d7055db6e4895ecc4dd490545d0c
Shawn <60294318+Shawn-sudo@users.noreply.github.com>
2022-09-17 00:49:58 -0400

    Update Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,21 +2,13 @@ import React from "react";
 import AppBar from "./AppBar";
 import Footer from "./Footer";
 
-//styles
-const pageStyles = {
-  fontFamily:
-    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen','Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',sans-serif",
-  WebkitFontSmoothing: "antialiased",
-  MozOsxFontSmoothing: "grayscale",
-};
-
 type Props = {
   children?: React.ReactNode;
 };
 
-const Layout = ({ children }: Props) => {
+function Layout(props: Props) {
   return (
-    <div style={pageStyles}>
+    <>
       <AppBar />
       <div
         style={{
@@ -24,11 +16,11 @@ const Layout = ({ children }: Props) => {
           marginTop: 55,
         }}
       >
-        {children}
+        {props.children}
       </div>
       <Footer />
-    </div>
+    </>
   );
-};
+}
 
 export default Layout;
