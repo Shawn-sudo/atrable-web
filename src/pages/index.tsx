5b080887aaa3b82c882ecf71111b65029b215b69
Shawn <60294318+Shawn-sudo@users.noreply.github.com>
2022-09-18 22:32:51 -0400

    New release

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,12 @@ import appScreenshot_myProfile1 from "../images/app_screenshots/IMG_7624.png";
 import appScreenshot_myProfile2 from "../images/app_screenshots/IMG_7625.png";
 import appScreenshot_friendsProfile from "../images/app_screenshots/IMG_7626.png";
 
-const IndexPage = () => {
+function IndexPage() {
   return (
     <Layout>
       <title>Atrable: Audio Required!</title>
       <main className="horizontal-margin">
+        {/* Audio based social media, Atrable */}
         <div
           style={{
             display: "flex",
@@ -23,12 +24,12 @@ const IndexPage = () => {
             alt="Atrable is based on audio"
             style={{
               maxHeight: "calc(75vh - 150px)",
-              borderRadius: 20,
               margin: "5vmin",
+              borderRadius: 20,
               boxShadow: "var(--shadow-big)",
             }}
           />
-          <div style={{ margin: 50 }}>
+          <div style={{ margin: "5vmin" }}>
             <h1 style={{ textAlign: "center" }}>
               Audio based social media, Atrable
             </h1>
@@ -42,15 +43,13 @@ const IndexPage = () => {
               <a
                 href="https://get.atrable.com"
                 className="Button colored-background"
-                style={{
-                  margin: "3vmin",
-                }}
+                style={{ margin: "3vmin" }}
               >
                 Get Atrable
               </a>
               <p
                 style={{
-                  color: "var(--primary-color)",
+                  color: "var(--hint-color)",
                   marginTop: "auto",
                   marginBottom: "auto",
                 }}
@@ -60,6 +59,8 @@ const IndexPage = () => {
             </div>
           </div>
         </div>
+
+        {/* No need to be fun */}
         <div
           style={{
             display: "flex",
@@ -67,29 +68,27 @@ const IndexPage = () => {
             justifyContent: "center",
           }}
         >
-          <div
+          <img
+            src={appScreenshot_recorder}
+            alt="My Profile"
             style={{
-              display: "flex",
+              maxHeight: "calc(75vh - 150px)",
+              width: "fit-content",
               margin: "5vmin",
+              borderRadius: 20,
+              boxShadow: "var(--shadow-big)",
             }}
-          >
-            <img
-              src={appScreenshot_recorder}
-              alt="My Profile"
-              style={{
-                maxHeight: "calc(75vh - 150px)",
-                borderRadius: 20,
-                boxShadow: "var(--shadow-big)",
-              }}
-            />
-          </div>
-          <div style={{ margin: 50 }}>
+          />
+
+          <div style={{ margin: "5vmin" }}>
             <h1 style={{ textAlign: "center" }}>No need to be fun</h1>
             <p style={{ textAlign: "center" }}>
-              Just like a diary, record your experiences with audio.{" "}
+              Just like a diary, record your experiences with audio.
             </p>
           </div>
         </div>
+
+        {/* Feel your past with audio */}
         <div
           style={{
             display: "flex",
@@ -124,16 +123,18 @@ const IndexPage = () => {
               }}
             />
           </div>
-          <div style={{ marginTop: 50 }}>
+          <div style={{ margin: "5vmin" }}>
             <h1 style={{ textAlign: "center" }}>Feel your past with audio</h1>
             <p style={{ textAlign: "start", maxWidth: 500 }}>
-              Audio records vibe of the moment. The sound of bell ringing,
-              people chattering, windows opening, and keyboards typing, they all
-              together form a beautiful vibe. Feel those when you're viewing
+              Audio records vibe of the moment. The sound of everything (bell
+              ringing, people chattering, windows opening, and keyboards typing)
+              all together form a beautiful vibe. Feel those while listening to
               your past posts.
             </p>
           </div>
         </div>
+
+        {/* Meet your friends through audio */}
         <div
           style={{
             display: "flex",
@@ -158,20 +159,75 @@ const IndexPage = () => {
               }}
             />
           </div>
-          <div style={{ marginTop: 50 }}>
-            <h1 style={{ textAlign: "center" }}>See your friends' past</h1>
+          <div style={{ margin: "5vmin" }}>
+            <h1 style={{ textAlign: "center" }}>
+              Meet your friends through audio
+            </h1>
             <p style={{ textAlign: "start", maxWidth: 500 }}>
-              This is actually the reason Atrable is made. On Instagram and
-              Facebook, I couldn't really see how my friends have been living
-              before meeting me, cause they upload only their happiest moment. I
-              could never know who they are with those posts. Requiring audio, I
-              want to know my friends' past vividly and relate with them better.
+              On traditional photo based social media (Instagram, Snapchat,
+              BeReal, vsco, ...), my friends only share their cool photos. With
+              those photos, I couldn't really know who they are. Using audio, we
+              get to know each other better.
             </p>
           </div>
         </div>
+        <div style={{}}>
+          <div style={{ margin: "5vmin" }}>
+            <h1 style={{ textAlign: "center" }}>People love Atrable 🤩</h1>
+            <p style={{ textAlign: "center" }}>I mean, I hope so</p>
+          </div>
+        </div>
+
+        {/* More: Developing Story */}
+        <a
+          href="https://seihyun.atrable.com/logs/atrable"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Button notcolored-background"
+          style={{
+            textDecoration: "none",
+            display: "flex",
+            flexWrap: "wrap",
+            width: "fit-content",
+          }}
+        >
+          <strong>Developing Story</strong>
+          <div
+            style={{
+              marginLeft: 15,
+              marginRight: 15,
+            }}
+          >
+            <strong>→</strong>
+          </div>
+        </a>
+
+        {/* More: Atrable Developer */}
+        <a
+          href="https://seihyun.atrable.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Button notcolored-background"
+          style={{
+            textDecoration: "none",
+            display: "flex",
+            justifyContent: "space-evenly",
+            width: "fit-content",
+          }}
+        >
+          <strong>More about the Developer</strong>
+          <div
+            style={{
+              marginLeft: 15,
+              marginRight: 15,
+            }}
+          >
+            <strong>→</strong>
+          </div>
+        </a>
       </main>
     </Layout>
   );
-};
+}
 
 export default IndexPage;
